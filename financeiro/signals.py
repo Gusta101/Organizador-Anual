a54e234495fb2e2231afd720e98d711493cfec8e
Gustavo Balbino <gu_gui14@hotmail.com>
2026-03-31 20:59:54 -0300

    (financeiro) limpeza total

--- financeiro/signals.py
+++ financeiro/signals.py
@@ -3,13 +3,3 @@ from django.dispatch import receiver
 from django.utils import timezone
 from django.db.models import Sum
 
-from .models import Transacao
-from .services import atualizar_progresso_objetivo, estornar_progresso_objetivo
-
-@receiver(post_save, sender=Transacao)
-def trigger_atualizar_progresso_objetivo(sender, instance, created, **kwargs):
-    atualizar_progresso_objetivo(instance)
-
-@receiver(post_delete, sender=Transacao)
-def trigger_estornar_progresso_objetivo(sender, instance, **kwargs):
-    estornar_progresso_objetivo(instance)
